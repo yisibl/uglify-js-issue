4b010bce1743af4e6e51f02f65ac3c1f31367f2a
一丝 <percyley@qq.com>
2024-08-06 15:54:41 +0800

    😄 init

---
+++ README.md
@@ -0,0 +1,14 @@
+# uglify-js-issue
+
+## What's the problem?
+
+uglify-js `3.19.0` causes an issue with compression losing code, the `.init=function()` function is missing from `build_3_19/day.js`.
+
+This does not happen in version 3.18.0, compare the difference between the files in `build_3_18/day.js` and `build_3_19/day.js`.
+
+## Steps
+
+```bash
+npm i
+npm run build
+```
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+// const uglify = require('gulp-uglify')
+const composer = require('gulp-uglify/composer')
+
+const uglifyjs_3_18 = require('uglifyjs-3.18.0')
+const minify_3_18 = composer(uglifyjs_3_18, console)
+
+const uglifyjs_3_19 = require('uglifyjs-3.19.0')
+const minify_3_19 = composer(uglifyjs_3_19, console)
+
+async function js() {
+    // uglify-js 3.18.0
+    gulp.src([
+      './input/day.js',
+    ])
+      .pipe(minify_3_18())
+      .on('error', function (err) { log.error(err.toString()) })
+      .pipe(gulp.dest('./build_3_18/'))
+
+    // uglify-js 3.19.0
+    gulp.src([
+        './input/day.js',
+      ])
+      .pipe(minify_3_19())
+      .on('error', function (err) { log.error(err.toString()) })
+      .pipe(gulp.dest('./build_3_19/'))
+}
+
+exports.default = gulp.series(js)
